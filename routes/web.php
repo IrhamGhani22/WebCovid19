d6483b21ea9434bc82dbafd0436ef85b4057a0dd
IrhamGhani22 <irhamag22@gmail.com>
2020-06-08 09:16:08 +0700

    Backend Searching Hospital User

--- routes/web.php
+++ routes/web.php
@@ -26,9 +26,6 @@ Route::get('/info', 'InfoController@info');
 Route::get('/about', 'ContactUsController@index');
 Route::post('/about', 'ContactUsController@store');
 
-Route::get('/berita', 'BeritaController@index2');
-Route::get('/berita/search', 'BeritaController@search');
-Route::get('/berita/{id}/detail', 'BeritaController@detail');
 
 Route::get('/detailBerita', function () {
     return view('detailBerita');
@@ -54,9 +51,15 @@ Route::get('/beritaAdmin/{id}/edit', 'BeritaController@edit')->middleware('auth'
 Route::patch('/beritaAdmin/{id}', 'BeritaController@update')->middleware('auth')->name('beritaAdminupdate');
 Route::delete('/beritaAdmin/{id}', 'BeritaController@destroy')->middleware('auth')->name('beritaAdmindestroy');
 
+Route::get('/berita', 'BeritaController@index2');
+Route::get('/berita/search', 'BeritaController@search');
+Route::get('/berita/{id}/detail', 'BeritaController@detail');
+
 Route::get('/hospitalAdmin', 'RujukController@index')->middleware('auth')->name('hospitalAdmin');
 Route::get('/hospitalAdmin/create', 'RujukController@create')->middleware('auth')->name('formAdmin');
 Route::post('/hospitalAdmin', 'RujukController@store')->middleware('auth')->name('hospitalAdminstore');
 Route::get('/hospitalAdmin/{id}/edit', 'RujukController@edit')->middleware('auth')->name('formAdminedit');
 Route::patch('/hospitalAdmin/{id}', 'RujukController@update')->middleware('auth')->name('formAdminupdate');
 Route::delete('/hospitalAdmin/{id}', 'RujukController@destroy')->middleware('auth')->name('formAdmindestroy');
+
+Route::get('/hospital/search', 'RujukController@search');
